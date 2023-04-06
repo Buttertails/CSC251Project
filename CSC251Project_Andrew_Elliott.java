a11b9619a6f1d4be31886f4b788ca0fe838f285d
Buttertails <drewe52769@gmail.com>
2023-04-05 21:35:17 -0400

    display data
    Added code to display all policy information, the number of smokers, and the number of non-smokers.

--- CSC251Project_Andrew_Elliott.java
+++ CSC251Project_Andrew_Elliott.java
@@ -94,5 +94,41 @@ public class CSC251Project_Andrew_Elliott
          Policy policy6 = new Policy(policyNumber, providerName, firstName, lastName, age, smoking, height, weight);          
          policyList.add(policy6);
       }
+      
+      for(int index = 0; index < policyList.size(); index++)
+      {
+         System.out.println("Policy Number: " + policyList.get(index).getPolicyNumber());
+         System.out.println("Provider Name: " + policyList.get(index).getProviderName());
+         System.out.println("Policyholder's First Name: " + policyList.get(index).getPolicyHolderFirstName());
+         System.out.println("Policyholder's Last Name: " + policyList.get(index).getPolicyHolderLastName());
+         System.out.println("Policyholder's Age: " + policyList.get(index).getPolicyHolderAge());
+         System.out.println("Policyholder's Smoking Status: " + policyList.get(index).getPolicyHolderSmokingStatus());
+         System.out.printf("Policyholder's Height: %.1f inches\n", Double.parseDouble(policyList.get(index).getPolicyHolderHeight()));
+         System.out.printf("Policyholder's Weight: %.1f pounds\n", Double.parseDouble(policyList.get(index).getPolicyHolderWeight()));
+         System.out.printf("Policyholder's BMI: %.2f\n", policyList.get(index).calcBMI());
+         System.out.printf("Policy Price: $%.2f\n", policyList.get(index).calcPolicyPrice());
+         System.out.println(); 
+      }
+      
+      policyInfo.close();
+     
+      File file1 = new File("PolicyInformation.txt");
+      Scanner policyInformation = new Scanner(file1);
+      
+      int numOfSmokers = 0;
+      int numOfNonSmokers = 0;
+      
+      while(policyInformation.hasNext())
+      {
+         String smokingString = policyInformation.nextLine();
+         
+         if(smokingString.equals("smoker"))
+            numOfSmokers++;
+         else if(smokingString.equals("non-smoker"))
+            numOfNonSmokers++;
+      }
+      
+      System.out.println("The number of policies with a smoker is: " + numOfSmokers);
+      System.out.print("The number of policies with a non-smoker is: " + numOfNonSmokers);
    }
 }
